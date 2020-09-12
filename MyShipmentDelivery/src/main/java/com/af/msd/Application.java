7055424600f33ae6cffdd9c32398216e93625dad
Ravi Jiyani <jiyaniravi@gmail.com>
2020-09-12 12:47:04 +0530

    Removed unused imports from Application.java

--- MyShipmentDelivery/src/main/java/com/af/msd/Application.java
+++ MyShipmentDelivery/src/main/java/com/af/msd/Application.java
@@ -1,14 +1,11 @@
 package com.af.msd;
 
-import com.af.msd.common.status.OrderStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        //SpringApplication.run(Application.class, args);
-
-        System.out.println(OrderStatus.UNKNOWN.getBy(2));
+        SpringApplication.run(Application.class, args);
     }
 }
